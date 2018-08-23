a825a06f5658ceec4768678359047a6d8728367c
Saufi Dzulkifley <saufi@Saufis-MacBook-Pro.local>
2018-08-23 21:28:37 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardsInPlay);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay);
-
-
-if (cardsInPlay.length === 2){
-	if ( cardsInPlay[0] === cardsInPlay[1]){
-		alert("You have found a match");
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2){
+		if ( cardsInPlay[0] === cardsInPlay[1]){
+			alert("You have found a match");
+		}
+		else {
+			alert("Sorry, Try again.");
+		}
 	}
-	else {
-		alert("Sorry, Try again.")
-	}
-}
+};
+
+var flipCard = function(cardId){
+
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
