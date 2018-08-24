af73dc84973c2c969ed65ddc79a7e89d62508ad4
Saufi Dzulkifley <saufi@Saufis-MacBook-Pro.local>
2018-08-24 19:31:05 +0800

    experimented with game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -14,57 +14,88 @@ var cards = [
 	suit: "hearts",
 	cardImage: "images/king-of-hearts.png"
 },
-{
-	rank: "king",
-	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
-}
+  {
+ 	rank: "king",
+ 	suit: "diamonds",
+ 	cardImage: "images/king-of-diamonds.png"
+    } 
 ];
+
+var startingCards = [];
 
 var cardsInPlay = [];
 
-// var random = function() {
-// 	Math.floor(Math.random() * cards.length);
-// };
+var removeOk = function(){
+	var main = document.getElementsByTagName("main")[0];
+	var okButton = document.getElementById("ok");
+	main.removeChild(okButton);	
+};
 
 var checkForMatch = function(){
+
 	if (cardsInPlay.length === 2){
 		if ( cardsInPlay[0] === cardsInPlay[1]){
 			alert("You have found a match");
 		}
 		else {
 			alert("Sorry, Try again.");
-		}
+		}	
+		 removeOk();
 	}
+	
 };
+
 
 var flipCard = function(){
 
 	var cardId = this.getAttribute('data-id')
 
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	console.log("User flipped " + startingCards[cardId]);
+	
+	cardsInPlay.push(startingCards[cardId]);
 
-	cardsInPlay.push(cards[cardId].rank);
-
-	this.setAttribute("src", cards[cardId].cardImage);
+	this.setAttribute("src", startingCards[cardId]);
 
 	checkForMatch();
-};
+
+};	
 
 var createBoard = function(){
 
-	for (var i = 0; i < cards.length; i++){
+	for (var i = 0; i < 12; i++){
 		var cardElement = document.createElement('img');
-		cardElement.setAttribute("src", "images/back.png");
+		var random = cards[Math.floor(Math.random() * 3)];
+		cardElement.setAttribute("src", random.cardImage);
+		startingCards.push(random.cardImage)
 		cardElement.setAttribute("data-id", i);
-		cardElement.addEventListener("click", flipCard);
 		document.getElementById("gameboard").appendChild(cardElement);
 	}
 
+	var backOfCards = function(){
+		for (var i = 0; i < 12; i++){
+		var changeToBack = document.getElementsByTagName('img')[i]
+		changeToBack.setAttribute("src", "images/back.png");
+		changeToBack.setAttribute("data-id", i);
+		changeToBack.addEventListener("click", flipCard);
+		}
+	};
+
+	var ok = document.getElementsByTagName("button")[0];
+	ok.addEventListener("click", backOfCards);
+
+
 };
 
-createBoard();
+var resetButton = document.getElementById("reset");
+var reset = function(){
+	location.reload();
+};
 
+resetButton.addEventListener("click", reset); 
+
+
+		
+
+
+createBoard();
 
